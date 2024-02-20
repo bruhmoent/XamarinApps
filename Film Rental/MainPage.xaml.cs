8e6d76baaec3d99e64e2c74b68578ece18b9efd1
bruhmoent <69918580+bruhmoent@users.noreply.github.com>
2024-02-20 23:11:50 +0100

    Add files via upload

---
+++ Film Rental/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Wyp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            int _dni = int.Parse(dni.Text);
+
+            int final = _dni * 20;
+            wynik.Text += " " + final.ToString() + " PLN.";
+
+        }
+    }
+}
